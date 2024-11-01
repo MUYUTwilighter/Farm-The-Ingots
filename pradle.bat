c646724098d7faea92b93cb15414899547170837
MUYU_Twilighter <1484605372@qq.com>
2024-11-01 15:27:15 -0400

    Update pradle.bat

--- pradle.bat
+++ pradle.bat
@@ -1,7 +1,13 @@
 @echo off
 
-if "%1" == "buildClient" (
-    python pradle/build_client.py
+if "%1" == "build" (
+    if "%2" == "client" (
+        python pradle/build_client.py
+    ) else if "%2" == "server" (
+        echo build server not available
+    ) else (
+        python pradle/build_client.py
+    )
 ) else if "%1" == "runServer" (
     python pradle/run_server.py
 )
