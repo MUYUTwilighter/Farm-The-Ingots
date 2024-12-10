2e1efc6d438851477e67500d2243914b67eccb11
MUYU_Twilighter <1484605372@qq.com>
2024-12-09 22:51:28 -0500

    1. Correct Iris version 2. Fix typo 3. Add support on how to remove floating inventory

--- pradle.bat
+++ pradle.bat
@@ -2,18 +2,18 @@
 
 if "%1" == "build" (
     if "%2" == "client" (
-        python pradle/build_client.py
+        python ./pradle/build_client.py
     ) else if "%2" == "server" (
-        python pradle/build_server.py
+        python ./pradle/build_server.py
     ) else (
-        python pradle/build_client.py
-        python pradle/build_server.py
+        python ./pradle/build_client.py
+        python ./pradle/build_server.py
     )
 ) else if "%1" == "runServer" (
-    python pradle/run_server.py
+    python ./pradle/run_server.py
 ) else if "%1" == "init" (
     shift
-    python pradle/init.py %*
+    python ./pradle/init.py %*
 ) else if not "%1" == "" (
     echo Unknown command "%1"
 ) else (
